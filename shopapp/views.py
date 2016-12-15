e520eb7da486235aba78cc9ab93a7f3ea029093f
Danrusu55 <daniel7rusu@gmail.com>
2016-12-15 15:13:56 -0800

    Modified template to show products

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Product
 
 def product_list(request):
-    return render(request, 'shopapp/product_list.html', {})
+    products = Product.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
+    return render(request, 'shopapp/product_list.html', {'products':products})
