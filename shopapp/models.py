4d5430662012fc900d4b0b5f3c34a3b24e18da08
Dan Rusu <danrusu@Users-MacBook-Air.local>
2016-12-15 12:18:54 -0800

    Fixed python version issue to 3.5

--- shopapp/models.py
+++ shopapp/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     image = models.ImageField(upload_to='pictures', blank=True, null=True)
-    
+
     def post(self):
         self.published_date = timezone.now()
 
