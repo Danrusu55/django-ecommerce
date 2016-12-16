1b2f8b8208e0f5a2e4fb3796926e4fd44adad507
Danrusu55 <daniel7rusu@gmail.com>
2016-12-15 16:38:09 -0800

    Added view and template for product page

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Product
 
 def product_list(request):
     products = Product.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    
+
     return render(request, 'shopapp/product_list.html', {'products':products})
+def product_detail(request, pk):
+    product = get_object_or_404(Product,pk=pk)
+    return render(request,'shopapp/product_detail.html', {'product': product})
